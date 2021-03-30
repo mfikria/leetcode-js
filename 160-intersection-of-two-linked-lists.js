0bba363c0542c691c38fa09d6f5726713b5296f0
M Fikri A <mfikria@gmail.com>
2021-03-30 13:03:15 +0700

    Create 160-intersection-of-two-linked-lists.js

---
+++ 160-intersection-of-two-linked-lists.js
@@ -0,0 +1,39 @@
+/**
+ * https://leetcode.com/problems/intersection-of-two-linked-lists/
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    let pointerA = headA;
+    let pointerB = headB;
+    let pointerIntersection = null;
+    while(pointerA && pointerB) {
+        if (pointerA.next === pointerIntersection && pointerB.next === pointerIntersection) {
+            if (pointerA !== pointerB) {
+                return pointerIntersection;
+            } else {
+                pointerIntersection = pointerA;
+                pointerA = headA;
+                pointerB = headB;
+            }
+        } else {
+            if (pointerA.next !== pointerIntersection) {
+                pointerA = pointerA.next;
+            }
+            if (pointerB.next !== pointerIntersection) {
+                pointerB = pointerB.next;
+            }   
+        }
+    }
+    
+    return pointerIntersection;
+};
